3cb28c1f0228f92771cb3131619308c1429859d2
Conor Mitchell <cjmit16@gmail.com>
2016-03-15 16:31:10 -0400

    Initial Upload - functional
    The code is functional but still requires some bug fixes and additional features.  These include:
     - resolve subprocess KeyboardInterupt error on Ctrl-C exit
     - add ETA estimation functionality
     - customize help/usage text for -h option
     - add option to omit .0 and .255 addresses? idk
     - TESTING
    
    In relationship to the bashping project on which this is based, this now includes a more functional way of creating the IP range.  This includes:
     - the ability to define starting and ending IPs down to the last octet (bashping would always ping from 0 to 255 on the last octet)
     - the ability to ping up to a full A-block (16.8 million IPs) in a single command execution
     - if only one IP is supplied, it will only ping that ip, unless the last octet is a '0', in which case it will ping the whole /24

---
+++ pingsweep.py
@@ -0,0 +1,160 @@
+#!/usr/bin/python
+
+### IPv4 Pingsweeper (Class B)
+### 
+### 
+
+## Imports
+
+import time
+import optparse
+import sys
+import subprocess
+
+
+## Define functions
+
+def validate_ip(ip):
+    a = ip.split('.')
+    if len(a) != 4:
+        return False
+    for x in a:
+        if not x.isdigit():
+            return False
+        i = int(x)
+        if i < 0 or i > 255:
+            return False
+    return True
+
+def parse_ip(ip):
+    a = ip.split('.')
+    return a
+
+def make_ip_list(ipBeg, ipEnd):
+    
+    ip_list = []
+    start = list(map(int, ipBeg.split(".")))
+    end = list(map(int, ipEnd.split(".")))
+    
+    if ipBeg == ipEnd:
+        if start[3] == 0:
+            end[3] = 255
+    
+    if (end[0] == start[0] and end[1] > start[1]) or (end[0] == start[0] and end[1] == start[1] and end[2] > start[2]) or (end[0] == start[0] and end[1] == start[1] and end[2] == start[2] and end[3] >= start[3]):
+        temp = start
+
+        ip_list.append(ipBeg)
+        while temp != end:
+            start[3] += 1
+            for i in (3, 2, 1):
+                if temp[i] == 256:
+                    temp[i] = 0
+                    temp[i-1] += 1
+            ip_list.append(".".join(map(str, temp)))
+
+        return ip_list
+    else:
+        print "Error - The second IP must be greater than or equal to the first IP."
+        print "'-h' option for help menu"
+        sys.exit()
+
+def make_grep_string(v, r):
+    grep_string = ""
+    if v:
+        if r:
+            grep_string = " 2>&1 | grep -F -v '1/1/0%'"
+        else:
+            grep_string = " 2>&1 | grep -F -v '1/0/100%'"
+    else:
+        if r:
+            grep_string = " 2>&1 | grep '1/0/100%' | cut -f1 -d' '"
+        else:
+            grep_string = " 2>&1 | grep '1/1/0%' | cut -f1 -d' '"
+    return grep_string
+
+
+## Get Options
+
+parser = optparse.OptionParser()
+
+parser.add_option('-v', '--verbose',
+                  dest="verbose",
+                  default=False,
+                  action="store_true",
+                  help='include all fping output',
+                 )
+parser.add_option('-r', '--reverse',
+                  dest="reverse",
+                  default=False,
+                  action="store_true",
+                  help='display failed pings instead of successful pings',
+                 )
+parser.add_option('-t', '--timeout',
+                  dest="timeout",
+                  default=200,
+                  help='define a ping timeout in miliseconds (default is 200)',
+                 )
+
+options, remainder = parser.parse_args()
+
+
+## Initialize variables
+
+verbose = options.verbose
+reverse = options.reverse
+timeout = options.timeout
+ipBeg = "none"
+ipEnd = "none"
+single = False
+
+
+
+if len(remainder) == 1:
+    if validate_ip(remainder[0]):
+        single = True
+        ipBeg = remainder[0]
+    else:
+        print "Invalid IP Address '%s'" % (remainder[0])
+        print "'-h' option for help menu"
+        sys.exit()
+elif len(remainder) == 2:
+    if validate_ip(remainder[0]):
+        ipBeg = remainder[0]
+    else:
+        print "Invalid IP Address '%s'" % (remainder[0])
+        print "'-h' option for help menu"
+        sys.exit()
+    if validate_ip(remainder[1]):
+        ipEnd = remainder[1]
+    else:
+        print "Invalid IP Address '%s'" % (remainder[1])
+        print "'-h' option for help menu"
+        sys.exit()
+else:
+    print "Invalid arguments. Use -h option for usage format."
+    sys.exit()
+    
+if single:
+    ipEnd = ipBeg
+
+ip_list = make_ip_list(ipBeg, ipEnd)
+
+print "Starting ping sweep on %s through %s...\n" % (ip_list[0], ip_list[-1])
+    
+if verbose:
+    print "[+] Verbose option set\n"
+
+if timeout != 200:
+    print "[+] Timeout set to %s milliseconds\n" % (timeout)
+
+if reverse:
+    print "[+] Reverse option set - displaying failed pings\n"
+
+print "Scan start: %s\n......" % (time.ctime())
+
+grep_string = ""
+for ip in ip_list:
+    bash_string = "fping -a -c1 -t%s %s%s" % (timeout, ip, make_grep_string(verbose, reverse))
+    subprocess.call(bash_string, shell=True)
+
+print "......\nScan finished at: %s" % (time.ctime())
